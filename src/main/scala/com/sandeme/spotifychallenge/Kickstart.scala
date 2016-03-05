94332d82c5a9b4323759ca7255f2304e04ae35d5
Michael Sanders <mesanders@mail.bradley.edu>
2016-03-05 09:14:27 -0600

    Initial commit after project setup

---
+++ src/main/scala/com/sandeme/spotifychallenge/Kickstart.scala
@@ -0,0 +1,10 @@
+package com.sandeme.spotifychallenge
+
+/**
+  * Created by sandeme on 3/5/16.
+  */
+object Kickstart {
+  def main(args: Array[String]) = {
+    println("Kickstart my heart");
+  }
+}
